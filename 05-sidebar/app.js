fce944272fabc639a525d87b3f28923a4e919f57
ayushballia <ayushtiwari@live.in>
2022-01-13 10:07:27 +0530

    Reviews

--- 05-sidebar/app.js
+++ 05-sidebar/app.js
@@ -4,6 +4,7 @@ const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
   sidebar.classList.toggle("show-sidebar");
+  
 });
 
 closeBtn.addEventListener("click", function () {
